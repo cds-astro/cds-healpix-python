493c46dfc033a41b216a24ef905d36757ad2e459
Matthieu Baumann <matthieu.baumann@astro.unistra.fr>
2019-01-23 17:29:18 +0100

    test benchmarking cone_search

--- cdshealpix/tests/test_benchmark_healpix.py
+++ cdshealpix/tests/test_benchmark_healpix.py
@@ -7,7 +7,8 @@ from ..healpix import lonlat_to_healpix, \
  healpix_to_lonlat, \
  healpix_to_skycoord, \
  healpix_vertices_lonlat, \
- healpix_neighbours
+ healpix_neighbours, \
+ cone_search_lonlat
 
 @pytest.mark.benchmark(group="lonlat_to_healpix")
 def test_lonlat_to_healpix(benchmark):
@@ -71,3 +72,19 @@ def test_healpix_neighbours(benchmark):
     ipixels = np.random.randint(12 * 4**(depth), size=size)
 
     neighbours = benchmark(healpix_neighbours, ipixels=ipixels, depth=depth)
+
+
+lon_c = np.random.rand(1)[0] * 360 * u.deg
+lat_c = (np.random.rand(1)[0] * 178 - 89) * u.deg
+radius_c = np.random.rand(1)[0] * 45 * u.deg
+depth_c = 5
+
+@pytest.mark.benchmark(group="cone_search")
+def test_cone_search(benchmark):
+    ipix, ipix_depth = benchmark(cone_search_lonlat, lon=lon_c, lat=lat_c, radius=radius_c, depth=depth_c)
+
+@pytest.mark.benchmark(group="cone_search")
+def test_cone_search_astropy(benchmark):
+    nside = 1 << depth_c
+
+    res = benchmark(astropy_healpix.core.healpix_cone_search, lon=lon_c, lat=lat_c, radius=radius_c, nside=nside, order="nested")
