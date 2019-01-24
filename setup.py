f99cfc3213dc2d742b2e302b10eb1342ac76162c
Matthieu Baumann <matthieu.baumann@astro.unistra.fr>
2019-01-24 11:04:29 +0100

    seek the dependencies in requirements.txt and the version in cdshealpix/version.py

--- setup.py
+++ setup.py
@@ -1,17 +1,24 @@
 from setuptools import setup
 from setuptools_rust import Binding, RustExtension
 
+from cdshealpix.version import __version__
+
+def get_package_dependencies():
+    dependencies = []
+    with open("./requirements.txt", "r") as f_in:
+        dependencies = f_in.read().splitlines()
+    return dependencies
+
 setup(
     name="cdshealpix",
-    version="0.1.2",
-    rust_extensions=[RustExtension("cdshealpix.cdshealpix", 'Cargo.toml', binding=Binding.NoBinding)],
+    version=__version__,
+    rust_extensions=[RustExtension(
+        "cdshealpix.cdshealpix",
+        'Cargo.toml',
+        # The binding with the Rust cdshealpix API is done using CFFI
+        binding=Binding.NoBinding)],
     packages=["cdshealpix"],
-    install_requires=[
-        # CFFI is used for loading the dynamic lib compiled with cargo
-        'cffi',
-        'numpy',
-        'astropy',
-    ],
+    install_requires=get_package_dependencies(),
     # rust extensions are not zip safe, just like C-extensions.
     zip_safe=False,
 )
