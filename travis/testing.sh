c5fc280cf2115a3aaa16b96c03a458dc9065fb40
Matthieu Baumann <matthieu.baumann@astro.unistra.fr>
2021-03-01 11:07:51 +0100

    try virtualenv for running maturin develop

--- travis/testing.sh
+++ travis/testing.sh
@@ -4,13 +4,20 @@
 set -e
 
 ### Run the tests ###
-# Download the dependencies for compiling cdshealpix
-$PIP install -r requirements-dev.txt
 # Install Rust compiler
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain nightly -y
 export PATH="$HOME/.cargo/bin:$PATH"
+
+$PIP install virtualenv
+virtualenv cdshealpixenv
+# Enter the new created python env
+source cdshealpixenv/bin/activate
+# Download the dependencies for compiling cdshealpix
+pip install -r requirements-dev.txt
 # Generate the dynamic library from the cdshealpix Rust crate.
 maturin develop --release
-$PYTHON -m pytest -v -s cdshealpix/tests/test_nested_healpix.py
-$PYTHON -m pytest -v -s cdshealpix/tests/test_ring_healpix.py
+python -m pytest -v -s cdshealpix/tests/test_nested_healpix.py
+python -m pytest -v -s cdshealpix/tests/test_ring_healpix.py
+# Go back to the native python env
+deactivate
 
