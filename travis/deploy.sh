1b8c339df49dd6514b3f2881f60185b9167e6e20
pineau <francois-xavier.pineau@astro.unistra.fr>
2021-03-01 11:58:05 +0100

    Fix pip install order in deploy.sh

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -9,15 +9,17 @@ if [[ $TRAVIS_TAG ]]; then
     ### Install Rust (no nighlty)
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain nightly -y
     ### Install virtualenv, maturin and twine
-    $PIP install virtualenv maturin twine
+    $PIP install virtualenv
     ### Create and activate a virtual env
     virtualenv cdshealpixenv
     source cdshealpixenv/bin/activate
+    ### Install maturin and twine
+    $PIP install maturin twine
     ### Build the wheels ###
     maturin build --release
     # maturin publish --username <username> --password <password> --repository-url <registry>?
     ### Upload the wheels to PyPI ###
-    $PYTHON -m twine upload --repository-url https://upload.pypi.org/legacy/ target/wheels/*.whl --skip-existing
+    python -m twine upload --repository-url https://upload.pypi.org/legacy/ target/wheels/*.whl --skip-existing
     ### Go back to the native python env
     deactivate
 fi
